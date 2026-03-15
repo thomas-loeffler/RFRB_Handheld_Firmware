9bbab120fdbaa63667fa9d420517de6f73fc90e7
thomas-loeffler <tommyryan2004@gmail.com>
2026-03-14 23:57:05 -0400

    forgot function in header lol

--- includes/mechanum_functions.h
+++ includes/mechanum_functions.h
@@ -17,4 +17,9 @@ void mechanum_driver(uint8_t raw_x, uint8_t raw_y,
                     float *fl, float *fr,
                     float *bl, float *br);
 
+int8_t round_and_cast(float x);
+
+
+void packet_packing(float fl, float fr, float bl, float br, uint8_t *payload);
+
 void mecanum_resultant_TEST(float fl, float fr, float bl, float br);
